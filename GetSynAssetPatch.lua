2cdeff0f22c82dbf75fd0fe6ea5f4aa29dd5ed5a
CandyWirus <amper123FS@gmail.com>
2022-02-23 11:17:11 -0700

    added a run-once

--- GetSynAssetPatch.lua
+++ GetSynAssetPatch.lua
@@ -1,45 +1,47 @@
-local folderName = "SynAssetStorage"
+if not islclosure(getsynasset) then
+	local folderName = "SynAssetStorage"
 
-if isfolder(folderName) then
-	delfolder(folderName)
-end
-
-makefolder(folderName)
-
-local function randomChar()
-	return math.random(0, 1) == 0 and string.char(math.random(65, 90)) or string.char(math.random(97, 122))
-end
-
-local function randomString(length)
-	local s = ""
-	for i = 1, length do
-		s = s .. randomChar()
-	end
-	return s
-end
-
-local oldGetAsset = getsynasset
-
-getgenv().getsynasset = function(url)
-	if isfile(url) then
-		return oldGetAsset(url)
-	end
-	local extension = string.split(url, ".")[#string.split(url, ".")]
-	local file = syn.request({
-		Url = url,
-		Method = "GET"
-	}).Body
-	local name
-	repeat
-		name = randomString(20) .. "." .. extension
-	until not isfile(folderName .. "/" .. name)
-	writefile(folderName .. "/" .. name, file)
-	local assetId = oldGetAsset(folderName .. "/" .. name)
-	return assetId, folderName .. "/" .. name
-end
-
-game.Close:Connect(function()
 	if isfolder(folderName) then
 		delfolder(folderName)
 	end
-end)
+
+	makefolder(folderName)
+
+	local function randomChar()
+		return math.random(0, 1) == 0 and string.char(math.random(65, 90)) or string.char(math.random(97, 122))
+	end
+
+	local function randomString(length)
+		local s = ""
+		for i = 1, length do
+			s = s .. randomChar()
+		end
+		return s
+	end
+
+	local oldGetAsset = getsynasset
+
+	getgenv().getsynasset = function(url)
+		if isfile(url) then
+			return oldGetAsset(url)
+		end
+		local extension = string.split(url, ".")[#string.split(url, ".")]
+		local file = syn.request({
+			Url = url,
+			Method = "GET"
+		}).Body
+		local name
+		repeat
+			name = randomString(20) .. "." .. extension
+		until not isfile(folderName .. "/" .. name)
+		writefile(folderName .. "/" .. name, file)
+		local assetId = oldGetAsset(folderName .. "/" .. name)
+		return assetId, folderName .. "/" .. name
+	end
+
+	game.Close:Connect(function()
+		if isfolder(folderName) then
+			delfolder(folderName)
+		end
+	end)
+end
