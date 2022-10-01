e97f60a11766cfbbb73b56402ca7a687aeeab4cf
CandyWirus <amper123FS@gmail.com>
2022-10-01 00:28:14 -0600

    Create UniverseViewerV3.lua

---
+++ UniverseViewerV3.lua
@@ -0,0 +1,49 @@
+--Andy_Wirus#5999 for issues
+--https://raw.githubusercontent.com/CandyWirus/scripts/master/UniverseViewerV3.lua
+
+local TeleportService = game:GetService("TeleportService")
+local HttpService = game:GetService("HttpService")
+
+local rw = RenderWindow.new("Universe Viewer")
+
+local placeId = game.PlaceId
+local gameId = game.GameId
+
+local function addPlace(name, id)
+	local line = rw:SameLine()
+	local tpButton = line:Button()
+	local copyButton = line:Button()
+	tpButton.Label = "Teleport"
+	copyButton.Label = "Copy ID"
+	tpButton.OnUpdated:Connect(function()
+		TeleportService:Teleport(id)
+	end)
+	copyButton.OnUpdated:Connect(function()
+		setclipboard(tostring(id))
+	end)
+	line:Label(name)
+end
+
+local cursor
+while true do
+	while true do
+		local success = pcall(function()
+			local list = HttpService:JSONDecode(game:HttpGet("https://develop.roblox.com/v1/universes/" .. gameId .. "/places?sortOrder=Asc&limit=100&cursor=" .. (cursor or "")))
+			local data = list.data
+			for i = 1, #data do
+				local placeData = data[i]
+				local newId = placeData.id
+				addPlace(placeData.name .. (placeId == newId and " - you are HERE" or ""), newId)
+			end
+			cursor = list.nextPageCursor
+		end)
+		if success then
+			break
+		end
+	end
+	if not cursor then
+		break
+	end
+end
+
+shared.UniverseViewerRenderWindow = rw
